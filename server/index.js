5d7713beee0fa678b94f3701ea1a139d0f05fc2a
unknown <quentindevial19@gmail.com>
2022-10-05 16:37:39 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://jean:123@cluster0.urpjt.gcp.mongodb.net/r18c20prod?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("CONNEXION DB OK !");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+const app = express();
+app.use(express.static(`${__dirname}/../client/build`));
+
+app.listen(80);
